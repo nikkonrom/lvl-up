1b49ca789ac6c8c24ec04eb245c8dbb4dbd600ec
nikkonrom <nikkonrom@gmail.com>
2017-10-28 14:28:38 +0300

    Added third lesson matireals and coursework

---
+++ Работа с проектами/ConsoleTorrent/ConsoleTorrent/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MonoTorrent.Client;
+using MonoTorrent.Common;
+
+namespace ConsoleTorrent
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect arguments given to input!");
+            }
+
+            var torrentFilePath = args[0];
+            var downloadFolderPath = args[1];
+
+            Download(torrentFilePath, downloadFolderPath);
+            Console.ReadKey();
+        }
+
+        private static void Download(string torrentFilePath, string downloadFolderPath)
+        {
+            Torrent torrent = Torrent.Load(torrentFilePath);
+
+            ClientEngine engine = new ClientEngine(new EngineSettings());
+            TorrentManager manager = new TorrentManager(torrent, downloadFolderPath, new TorrentSettings());
+            
+            engine.Register(manager);
+            manager.Start();      
+        }
+    }
+}
